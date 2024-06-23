709856d82888a80ece291cb0ea45d07e513fd295
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-06-23 23:18:29 +0900

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,1 +1,17 @@
+function selectTrigger() {
+    document.getElementById('SelectSound').pause();
+    document.getElementById('SelectSound').currentTime = 0;
+    ring();
+}
+function select() {
+   document.getElementById("SelectSound").play();
+}
 
+function pressTrigger() {
+    document.getElementById('PressSound').pause();
+    document.getElementById('PressSound').currentTime = 0;
+    press();
+}
+function press() {
+   document.getElementById("PressSound").play();
+}
